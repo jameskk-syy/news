45c9c4d49527a2e582b05fa6f75f2bcc42509c86
jameskk-syy <ronaldoeden70@gmail.com>
2024-11-25 19:37:44 +0300

    updated

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  const storedEmail = "albosanifatima355@gmail.com
+  const storedEmail = "albosanifatima355@gmail.com"
   ";
   const storedPassword = "fatima11-11";
 
